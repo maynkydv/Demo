c7ed7eb5b631ef6c4315c8d72e688adcc689ce4e
Mayank Yadav <myadav@Mayanks-MacBook-Air.local>
2024-08-16 18:36:11 +0530

    Revert "stash , commit stash apply"
    This reverts commit c718fcc28a08f3d0862b0e526022aeddc7e95aef.

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ const app = express() ;
 
 // To dos : node is not installed need to install it
 app.listen(3000, ()=>{ console.log("Server is running on Port 3000")});
-
-
-// making these changes to work with revert reset and stash commands
