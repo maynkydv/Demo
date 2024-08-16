6e42b487f320fbea988c1fbe5049940317fd6352
Mayank Yadav <myadav@Mayanks-MacBook-Air.local>
2024-08-16 15:44:20 +0530

    learning about git config

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require ('express');
 
 const app = express() ;
 
+
+// To dos : node is not installed need to install it
 app.listen(3000, ()=>{ console.log("Server is running on Port 3000")});
